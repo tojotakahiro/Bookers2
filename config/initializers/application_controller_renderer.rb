50832c4a4f59542f4be49a5e19bf543a4d32709e
tojotakahiro <t.0725.tennis@gmail.com>
2021-07-20 12:46:06 +0000

    [finish]”4-3”

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'c4644df6804bbe4310cb4ac85e14494dca8ba73738493742924f2f163364c6bf1d9a487aaba3c954b2ad12379454d836c4378c1798795434043daf6b72ec9c02'
